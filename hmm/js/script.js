f155571099bc00a1a52b6efbc4a12e307a3f6757
SHARONLEE94 <119920067+SHARONLEE94@users.noreply.github.com>
2022-12-28 00:16:14 +0900

    221227

--- hmm/js/script.js
+++ hmm/js/script.js
@@ -1,17 +1,25 @@
 $(function() {
 
 //scroll Event
+var $p2Title = $("#p2_title");
+var $p4Span = $("#p4");
+
   $(window).on("scroll",function(){
       var pos = $("html").scrollTop();
       
-      if(pos>=($("#p2_title").offset().top)*-0.8){
+      if(pos >= ($p2Title.offset().top)*-0.7){
           var index = 0;
 
           window.setInterval(function(){
               if(index == 5) return;
               $("#circle>div").eq(index).addClass("move");
               index++;
-          },700);
+          },600);
+      }
+      
+      //active
+      if(pos >= ($p4Span.offset().top)*0.7){
+        $("#p4Wrap > span").addClass("active");
       }
   });
 
@@ -22,4 +30,7 @@ $(function() {
 
     $this.toggleClass("change");
   });
+
+
+
 });
---
+++ hmm/test/main.js
@@ -0,0 +1,11 @@
+let user = {
+  name:"happi",
+  age :85,
+
+};
+
+// console.log(user);
+console.log("== user.age ==");
+console.log(user.age);
+console.log("== user.name ==");
+console.log(user.name);
